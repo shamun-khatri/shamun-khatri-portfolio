003f455e4cd1c9cebaffca453d376ef7616d5cb2
shamun-khatri <shamunkhatri@gmail.com>
2023-11-27 18:49:59 +0530

    Completed My Portfolio Website

---
+++ src/data/constants.js
@@ -0,0 +1,291 @@
+export const Bio = {
+  name: "Shamun Khatri",
+  roles: [
+    "Frontend Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning especially in Frontend development. I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/shamun-khatri",
+  resume:
+    "https://drive.google.com/file/d/1Yeqh5RoifT2yGjo38Tmdn2w9IQAy1dKV/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/shamun-khatri/",
+  twitter: "https://twitter.com/shamunkhatri",
+  // insta: "",
+  facebook: "https://www.facebook.com/skkhatri11/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          require('../images/express.png'),
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  // {
+  //   title: "Android",
+  //   skills: [
+  //     {
+  //       name: "Java",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+  //     },
+  //     {
+  //       name: "Kotlin",
+  //       image:
+  //         "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+  //     },
+  //     {
+  //       name: "XML",
+  //       image:
+  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+  //     },
+  //     {
+  //       name: "Android Studio",
+  //       image:
+  //         "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+  //     },
+  //   ],
+  // },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Wordpress",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/0c/Wordpress_logo_8.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://assets.vercel.com/image/upload/front/favicon/vercel/180x180.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Shopify",
+        image:
+          "https://cdn3.iconfinder.com/data/icons/social-media-2068/64/_shopping-512.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://files.jotform.com/jotformapps/0d51c1a6dc78d20137f6ac7d98eca9f9.png",
+    role: "Web Designer Freelancer",
+    company: "Fiverr, Upwork.",
+    date: "Mar 2020 - Present",
+    desc: "Created many websites using Wordpress, Shopify and helped them on their business requirements.",
+    skills: [
+      "Wordpress",
+      "Shopify",
+      "HTML",
+      "Basic CSS",
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://kvranalytics.com/wp-content/uploads/2022/12/updated_logo.jpg",
+    role: "Frontend Engineer Intern | Business Research",
+    company: "KVR Analytics",
+    date: "Jan 2023 - July 2023",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "Wordpress",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Django",
+    ],
+    doc: "",
+  },
+  {
+    id: 2,
+    img: require("../images/PerfectMall.png"),
+    role: "Ecommerce Dropshipping Business",
+    company: "PerfectMall",
+    date: "March 2021 - Nov 2023",
+    desc: "Started a ecommerce dropshipping business and I was doing everything from product research, Marketin, sales to crating and optimizing and ads creative. Due to lack of funds and too much RTO I have to shut down the business.",
+    skills: [
+      "Shopify",
+      "Facebook Ads",
+      "Google analytics",
+      "Facebook Pixel",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://images.collegedunia.com/public/college_data/images/logos/1490189804logoedt.png?h=80&w=80&mode=stretch",
+    school: "Jawaharlal Institute Of Technology, Borawan",
+    date: "Jul 2019 - May 2023",
+    grade: "7.5 CGPA",
+    desc: "I Completed B.Tech degree in Electronics and communication engineering at Jawaharlal institute of technology, Borawan.",
+    degree: "Bachelor of Technology - BTech, Electonics And Communication Engineering",
+  },
+  {
+    id: 1,
+    img: "https://pbs.twimg.com/profile_images/969683939852890113/IOBmiuTe_400x400.jpg",
+    school: "Anshul Vidhya Mandir, Jobat",
+    date: "2019",
+    grade: "44.4%",
+    desc: "I completed my class 12 high school education at Anshul Vidhya Mandir, Jobat, where I studied Maths with Physical Education.",
+    degree: "CBSE(XII), Maths With Physical Education",
+  },
+  {
+    id: 2,
+    img: "https://pbs.twimg.com/profile_images/969683939852890113/IOBmiuTe_400x400.jpg",
+    school: "Anshul Vidhya Mandir, Jobat",
+    date: "2017",
+    grade: "74%",
+    desc: "I completed my class 10 education at Anshul Vidhya Mandir,Jobat.",
+    degree: "CBSE(X), Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Movix",
+    date: "",
+    description:
+      "Movix is a movie exploration website where you can see the ratings of movies and filter out with genres and languages. It is made with React JS, Redux and SCSS. It uses the TMDB API to fetch the data. It also has a search bar to search for a particular movie. And can filter out with genres and languages.",
+    image:
+      "https://user-images.githubusercontent.com/100433320/283033735-683c51ed-112b-4a26-8a38-a59e3e252269.jpg",
+    tags: [
+      "React Js",
+      "Redux Toolkit",
+      "SCSS",
+      "TMDB API",
+      "Axios",
+      "React Router",
+      "Vercel"
+    ],
+    category: "web app",
+    github: "https://github.com/shamun-khatri/Movix",
+    webapp: "https://movix-shamun.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "Trello Clone",
+    date: "",
+    description:
+      "A seamless drag and drop interface to organize your day-to-day tasks. It is made with React JS, Next JS, Tailwind CSS, Zustand, Appwrite. It uses the Appwrite API to store and fetch the data and images. It also has a search bar to search for a particular task.",
+    image:
+      "https://user-images.githubusercontent.com/100433320/283044517-4d8b1437-c44a-4f15-a31a-a7b455f262f4.jpg",
+    tags: ["React Js", "Appwrite", "Next Js", "Tailwind Css", "Zustand"],
+    category: "web app",
+    github: "https://github.com/shamun-khatri/trello-clone",
+    webapp: "https://trellofollow.vercel.app/",
+    // member: [
+    //   {
+    //     name: "",
+    //     img: "",
+    //     linkedin: "",
+    //     github: "",
+    //   },
+    // ],
+  },
+  {
+    id: 1,
+    title: "Case Converter | A Text Utility App",
+    date: "",
+    description:
+      "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+    image:
+      "https://user-images.githubusercontent.com/100433320/283047266-84803cb7-d1dc-4379-a5d8-dc9984ca961a.jpg",
+    tags: [
+      "React Js",
+      "Bootstrap",
+      "Vercel",
+    ],
+    category: "web app",
+    github: "https://github.com/shamun-khatri/Text-Utility",
+    webapp: "https://casesconvert.vercel.app/",
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
